af488e6917e73e114ec98b5ce9294f95633fdd6d
SchneeBrocken3 <50659462+SchneeBrocken3@users.noreply.github.com>
2020-11-05 11:25:14 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -6,7 +6,7 @@
 int main(){
 
 	float num1;
-	int num2;
+	float num2;
 	float ans;
 	char op;
 	printf("\nEnter an operator (+,-,/,*): \n");
@@ -20,12 +20,21 @@ int main(){
 		case '+' : 
 			ans = num1 + num2;
 			break;
+		case '-' : 
+			ans = num1 - num2;
+			break;
+		case '/' : 
+			ans = num1 / num2;
+			break;
+		case '*' : 
+			ans = num1 * num2;
+			break;
 		default:
 			printf("Enter a correct operator!");
 			return 1;
 	
 	}
 	printf("\n The result is as follows:\n");
-	printf("%f %c %f = %f",num1, op ,num2,ans);
+	printf("%f %c %f = %f",num1, op ,num2, ans);
 	return 0;
 }
